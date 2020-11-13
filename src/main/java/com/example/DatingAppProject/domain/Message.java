6f071a2fa8b9837b79bc435d0b922bc6b2947d34
WafaaAjan <ou_ajan@hotmail.com>
2020-11-13 23:43:00 +0100

    ændringer i messagesr

--- src/main/java/com/example/DatingAppProject/domain/Message.java
+++ src/main/java/com/example/DatingAppProject/domain/Message.java
@@ -1,4 +1,49 @@
 package com.example.DatingAppProject.domain;
 
 public class Message {
+    private String firstName;
+    private String lastName;
+    private String message;
+
+
+    public Message(String firstName, String lastName, String message) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.message = message;
+
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/DatingAppProject/domain/MessageController.java
+++ src/main/java/com/example/DatingAppProject/domain/MessageController.java
@@ -1,4 +1,32 @@
 package com.example.DatingAppProject.domain;
 
-public class messageController {
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.ArrayList;
+
+public class MessageController {
+    private DataFacade facade = null;
+
+    public MessageController(DataFacade facade) {
+        this.facade = facade;
+    }
+
+
+    public ArrayList<User> getConversations(int id) throws DefaultException {
+        return facade.getConversations(id);
+    }
+
+    public Model packageUser(User user, Model model) throws DefaultException {
+        model.addAttribute("firstName", user.getFirstName());
+        model.addAttribute("lastName", user.getLastName());
+        return model;
+    }
+    /*
+    public Model packageMessage(Message message, Model model) throws DefaultException {
+        model.addAttribute("message", message.getMessage();
+        return model;
+    }
+
+
+     */
 }
